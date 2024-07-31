8bb75b9e15483439fab04cfed253fe76ea8525dd
Oskari Martela <omartela@c3r4p5.hive.fi>
2024-07-31 08:51:50 +0300

    Fixes ft_realloc that it frees the old pointer if malloc fails to allocate memory for new pointer

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:15:09 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/27 15:17:54 by omartela         ###   ########.fr       */
+/*   Updated: 2024/07/31 08:51:25 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,15 +18,14 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	if (old_size < new_size)
-	{
 		copy_size = old_size;
-	}
 	else
-	{
 		copy_size = new_size;
-	}
 	if (ptr)
 	{
 		ft_memcpy(new_ptr, ptr, copy_size);
